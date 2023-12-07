9993d32836893de4dded9c14683674ecf95067e7
Rafael Barros <137620663+rafabarros95@users.noreply.github.com>
2023-12-07 11:34:12 +0100

    About section updated

--- about.adm.php
+++ about.adm.php
@@ -26,8 +26,7 @@
     </header>
     <main>
         <h2>About us</h2>
-        <p><span>Flex</span><span id="fit">Fit</span> is a system able to offer Costumers the possibility to book a course of their choice without being registered at a specific Gym under Membership basis. That means that anyone who wants to do a course like Yoga, Pilates, Art Matials and so on, can easily choose which one they would like to do it and where, it shows the price as well. </p>
-        <p>Interested? <a href="registration.html">Register now</a> and start your course :)</p>
+        <p><span>Flex</span><span id="fit">Fit</span> is a system able to offer Costumers the possibility to book a course (Daily courses) of their choice without being registered at a specific Gym under Membership basis. That means that anyone who wants to do a course like Yoga, Pilates, Art Matials and so on, can easily choose which one they would like to do it and where, it shows the price as well. </p>
     </main>
 </body>
 </html>
